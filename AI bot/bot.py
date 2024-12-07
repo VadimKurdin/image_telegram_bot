8b0c1a50ef358b9b4e1fe1eaf95d3929e3d6c4fe
VadimKurdin <vkurdin23@gmail.com>
2024-12-07 23:48:07 +0300

    Add files via upload

---
+++ AI bot/bot.py
@@ -0,0 +1,32 @@
+import telebot
+from Logic_AI import getclass
+token = '8179192722:AAE_30ghptzSqaT2yd8qzfW2BcDCm0_hJ9w'
+
+# Замени 'TOKEN' на токен твоего бота
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /photo ")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+# Запускаем бота
+
+@bot.message_handler(content_types=['photo'])
+def ai_class(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    file_name = file_info.file_path.split('/')[-1]
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    result = getclass(file_name)
+    bot.reply_to(message, result)
+
+# Запускаем бота
+bot.polling()
